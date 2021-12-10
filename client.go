b2956c9551f65146bddb3f29d194d959f52c4134
JoshWellPharma <82033495+JoshWellPharma@users.noreply.github.com>
2021-12-10 16:25:37 +0000

    feat: requests are made with unique id (#8)
    Co-authored-by: Josh Dando <joshdando@Joshs-MacBook-Pro.local>

--- client.go
+++ client.go
@@ -44,6 +44,8 @@ import (
 	"net/url"
 	"strings"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type Client struct {
@@ -125,6 +127,8 @@ func (c *Client) newRequest(method, path string, body interface{}) (*http.Reques
 	}
 	req.Header.Set("Accept", "application/json")
 	req.Header.Set("User-Agent", c.UserAgent)
+	// Every request to NHS API should contain a unique id otherwise we receive a 429
+	req.Header.Set("X-Request-ID", uuid.New().String())
 	return req, nil
 }
 
@@ -150,5 +154,8 @@ func (c *Client) do(ctx context.Context, req *http.Request, v interface{}) (*Res
 	}
 	defer resp.Body.Close()
 	err = json.NewDecoder(resp.Body).Decode(v)
-	return newResponse(resp), err
+
+	r := newResponse(resp)
+	r.RequestID = req.Header.Get("X-Request-ID")
+	return r, err
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/Joshswooft/nhs v0.2.0
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/go-querystring v1.1.0
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/stretchr/testify v1.7.0
 )
